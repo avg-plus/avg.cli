ddfdea8e0cb22d018a76cf17affce6eaeb4bd95c
rexxara <15502815072@163.com>
2018-09-30 14:00:22 +0800

    add add platform part

--- src/cli.ts
+++ src/cli.ts
@@ -2,6 +2,7 @@
 import program from "commander"
 import path from "path"
 import child_process from 'child_process'
+import fs from 'fs'
 const liveServer = require('live-server')
 
 export default class CLI {
@@ -14,24 +15,48 @@ export default class CLI {
       .description("Create an AVGPlus project.")
       .action(async (name, options) => {
         const targetPath = path.resolve(process.cwd(), name);
-        child_process.spawn('cp', ['-r', path.resolve(`${__dirname}/../release`), targetPath])
+        child_process.spawn('cp', ['-r', path.resolve(__dirname, '..', 'release'), targetPath])
         console.log(`Creating project '${name}' on current directory ${targetPath}`)
         console.log(`Run cd ${name} && avg-cli run web `)
       })
 
     program
-      .command("plantform-add <plantform>")
-      .description("Add a plantform")
-      .action((plantform, options) => {
-        console.log(`Add a plantform '${plantform}'`)
+      .command("platform <add> [platform]")
+      .description("Add a platform")
+      .action((add, platform, options) => {
+        const table: any = {
+          win32: 'win-x86.zip',
+          darwin: 'darwin.zip',
+          linux: 'linux-x86.zip',
+          web: 'web.zip'
+        }
+        const bundlesDir = 'platform-bundles';
+        if (platform === 'desktop') {
+          const bundleSrc = path.resolve(process.cwd(), bundlesDir, table[process.platform])
+          if (fs.existsSync(bundleSrc)) {
+            console.log('Desktop bundles aready been installed.')
+          } else {
+            console.log('fetching bundles..')
+            child_process.spawn('cp', ['-r', path.resolve(__dirname, '..', bundlesDir, table[process.platform]), path.resolve(process.cwd(), bundlesDir)])
+          }
+        }
       })
     program
-      .command("run <plantform>")
+      .command("run <platform>")
       .description("Run current project")
-      .action((plantform, options) => {
-        console.log(path.resolve(process.cwd()))
-        liveServer.start()
-        console.log(`Run current project '${plantform}'`)
+      .action((platform, options) => {
+        switch (platform) {
+          case 'web':
+            console.log(path.resolve(process.cwd()))
+            liveServer.start()
+            console.log(`Run current project '${platform}'`)
+            break;
+          case 'desktop':
+            console.log('desktop')
+            break;
+          default:
+            break;
+        }
       })
 
     program.parse(argv)
